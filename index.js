8aa0fd6aef5aebfd08bc6a6d4950b422f7016c38
lasindu-demel <lasindudemel@gmail.com>
2020-11-22 08:59:11 +0530

    cors enabled

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const { request, response } = require('express')
 const app = express()
 const port = 3000
 
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Conrol-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 
 app.unsubscribe(bodyParser.json())
